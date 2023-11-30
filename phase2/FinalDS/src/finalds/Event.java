68182f7336424c5dd579c7c6432043f073a725e1
AbeerAlnafaei <144719854+AbeerAlnafaei@users.noreply.github.com>
2023-11-30 20:50:47 +0300

    Event class

--- phase2/FinalDS/src/finalds/Event.java
+++ phase2/FinalDS/src/finalds/Event.java
@@ -6,60 +6,64 @@ package finalds;
 
 
     
-    public class Event implements Comparable<Event> {
-//attributes
-    private String Title;
-    private String date;
-    private String time;
-    private String location;
-    LinkedList<Contact> contcts_Name;
+public class Event implements Comparable<Event> {
+    String title;
+    String date;
+    String time;
+    String location;
+    boolean EventType;  // event true , appointment = false;
+    LinkedList <Contact> contcts_Name;
 
-    public Event(String Title, String date, String time, String location, Contact c) {
-        this.Title = Title;
+    public Event(String title, String date, String time, String location, boolean t, Contact c) {
+        this.title = title;
         this.date = date;
         this.time = time;
         this.location = location;
-        contcts_Name = new LinkedList<Contact>();
+        this.EventType =t;
+        this.contcts_Name = new LinkedList<Contact> ();
         this.addContactInEvent(c);
     }
 
     public Event() {
-        this.Title = "";
-        this.date = "";
+        this.title = "";
+        this.date = null;
         this.time = "";
         this.location = "";
-        contcts_Name = new LinkedList<Contact>();
-
+        this.EventType = true;
+        this.contcts_Name = new LinkedList<Contact> ();
     }
 
     public boolean addContactInEvent(Contact c) {
-
-
-       if(contcts_Name.insertSort(c)) 
-        return true;
-       else
-           return false;
+             
+    
+        if ((this.EventType == true) || ((this.EventType == false)&&(contcts_Name.size == 0)))
+            return contcts_Name.insertSort(c);
+        
+        System.out.println("Could not add more than contact for an appoinment");
+        return false;
+  
 
     }
 
     public boolean removeContactFromEvent(Contact c) {
 
         Contact tmp = contcts_Name.remove(c);
-        if (tmp != null) {
+        if (tmp != null) 
             return true;
-        } else {
+      
             return false;
-        }
+        
 
     }
 
     //setter and getter
+
     public String getTitle() {
-        return Title;
+        return title;
     }
 
-    public void setTitle(String Title) {
-        this.Title = Title;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDate() {
@@ -86,31 +90,73 @@ package finalds;
         this.location = location;
     }
 
+    public boolean isEventType() {
+        return EventType;
+    }
+
+    public void setEventType(boolean EventType) {
+        this.EventType = EventType;
+    }
+
+    public LinkedList<Contact> getContcts_Name() {
+        return contcts_Name;
+    }
+
+    public void setContcts_Name(LinkedList<Contact> contcts_Name) {
+        this.contcts_Name = contcts_Name;
+    }
+    
+
+
+
+
     @Override
     public String toString() {
-        String str = "\nEvent title: " + Title
-                + "\nEvent date and time (MM/DD/YYYY HH:MM): " + date+" " + time
-                + "\nEvent location: " + location + "\n";
-            /*    + "Contacts names:\n";
-
-        contcts_Name.findFirst();
-        for (int i = 0; i < contcts_Name.size; i++) {
-            str = str+ contcts_Name.retrieve().getContact_name() + "\n";
-            contcts_Name.findNext();
+        String EventT = (this.EventType == true)? "Event ": "Appoinment ";     
+        String str = "\nEvent title: " + title +
+                    "\nEvent date and time (MM/DD/YYYY HH:MM): " + date+" " + time +
+                   "\nEvent location: " + location + "\n" +
+                   "\nType: " + EventT + "\n" +
+                    "\nContacts names:   \n" + contcts_Name.toString();
+                
+          return str;
+    }
+    
+            public void Print() {
+        String EventT = (this.EventType == true)? "Event ": "Appoinment ";     
+        String str = "\nEvent title: " + title +
+                    "\nEvent date and time (MM/DD/YYYY HH:MM): " + date + time +
+                   "\nEvent location: " + location + "\n" +
+                   "\nType: " + EventT + "\n" +
+                    "\nContacts names:   \n"  + contcts_Name.toString();
+                
+            System.out.println(str);
+    }
+@Override
+    public int compareTo(Event obj) {
+        try {
+            return (this.title.compareToIgnoreCase(obj.title));
         }
-        if( contcts_Name.size == 0)
-            str+="\nThere is no Contacts ";*/
-        return str;
+        catch (Exception e)
+        {
+             //To change body of generated methods, choose Tools | Templates.
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
     }
 
-    public int compareTo(String s) {
-        return this.Title.compareTo(s);
+    public boolean compareToSameEvent(Event obj) {
+        try {
+            return ((this.title.compareToIgnoreCase(obj.title) == 0) && 
+                    (this.date.compareTo(obj.date) == 0) &&
+                    (this.time.compareToIgnoreCase(obj.time) == 0) && (this.EventType == obj.EventType));
+        }
+        catch (Exception e)
+        {
+             //To change body of generated methods, choose Tools | Templates.
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
     }
-
-    public int compareTo(Event e) {
-        return this.Title.compareTo(e.Title);
-    }
-
+    
 }
     
 
